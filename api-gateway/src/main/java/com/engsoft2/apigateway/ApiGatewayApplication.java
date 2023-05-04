620027a573579839437d3c10f9442e6144a9715a
FELIPE ELSNER SILVA <f.elsner@facin.portoalegre.pucrsnet.br>
2023-05-04 19:41:36 -0300

    feat: tutorial

--- api-gateway/src/main/java/com/engsoft2/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/engsoft2/apigateway/ApiGatewayApplication.java
@@ -3,8 +3,17 @@ package com.engsoft2.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@RestController
 @SpringBootApplication
 public class ApiGatewayApplication {
+
+	@Autowired
+	private CircuitBreacker circuitBreacker;
+  
+	@RequestMapping("/currency-conversion")
+	public Mono<String> conversionCurrency() {
+	  return circuitBreacker.readingList();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
